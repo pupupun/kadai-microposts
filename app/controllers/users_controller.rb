b2b105813e5563209a33fbd46c70790bfc588091
shio.chika <shipoo2222@gmail.com>
2018-08-08 14:07:33 +0000

    micropost_favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
   def index
     @users=User.all.page(params[:page])
   end
@@ -36,6 +36,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
     counts(@user)
+  end
+  
+  def likes
+    @user = User.find(params[:id])
+    @favorites = @user.favorite_microposts.page(params[:page])
+    counts(@user)
+    render :favorite
   end
 
   private
